839741d933ab21bf5e6c8d999edd37198197eb2c
t0d0cyc <tanay.doshi@walmart.com>
2025-03-18 15:12:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,39 +4,39 @@ import AutoComplete from "./components/AutoComplete";
 import options from "./components/Suggestions";
 import * as Icon from "@livingdesign/icons-indigo";
 
-const App: React.FC = () => {
-  const [value, setValue] = useState<string>("");
+const App = () => {
+const [value, setValue] = useState<string>("");
 
-  const autoComplete = value === "" ? [] : options
-    .filter((option) => option.toLowerCase().startsWith(value.toLowerCase()))
-    .slice(0, 10);
+const autoComplete = value === "" ? [] : options
+  .filter((option) => option.toLowerCase().startsWith(value.toLowerCase()))
+  .slice(0, 10);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
+const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  setValue(event.target.value);
+};
 
-  const handleClick = () => {
-    setValue("");
-  };
+const handleClick = () => {
+  setValue("");
+};
 
-  return (
-    <>
-      <AutoComplete
-        value={value}
-        setValue={setValue}
-        autoComplete={autoComplete}
-        label="Auto-Complete Suggestions"
-        handleChange={handleChange}
-        handleClick={handleClick}
-        leading = {<Icon.Search size="small"/>}
-        trailing={<Icon.ChevronRight size="small"/>}
-        inputFieldStyle={{
-          width: '250px',
-          paddingRight: '33px'
-        }}
-      />
-    </>
-  );
+return (
+    <AutoComplete
+      value={value}
+      setValue={setValue}
+      autoComplete={autoComplete}
+      label="Auto-Complete Suggestions"
+      handleChange={handleChange}
+      handleClick={handleClick}
+      leading = {<Icon.Search size="small"/>}
+      trailing={
+          <Icon.ChevronRight size="small"/>
+      }
+      inputFieldStyle={{
+        width: "100%",
+        paddingRight: "30px",
+      }}
+    />
+);
 };
 
 export default App;
