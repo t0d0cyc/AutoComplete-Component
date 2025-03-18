adc818333e6135408af5514840d43119d3e76c41
t0d0cyc <tanay.doshi@walmart.com>
2025-03-18 11:50:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React, { useState, ChangeEvent } from "react";
 import AutoComplete from "./components/AutoComplete";
 import options from "./components/Suggestions";
 import * as Icon from "@livingdesign/icons-indigo";
-import { Button } from "@livingdesign/react";
 
 const App: React.FC = () => {
   const [value, setValue] = useState<string>("");
@@ -29,15 +28,11 @@ const App: React.FC = () => {
         label="Auto-Complete Suggestions"
         handleChange={handleChange}
         handleClick={handleClick}
-        leading={<Icon.Pencil size="small" />}
-        trailing={
-          <Button UNSAFE_style={{ height: "20px", width: "60px" }} trailing={<Icon.Box />}>
-            click
-          </Button>
-        }
+        leading = {<Icon.Search size="small"/>}
+        trailing={<Icon.ChevronRight size="small"/>}
         inputFieldStyle={{
           width: '250px',
-          paddingRight: '30px'
+          paddingRight: '33px'
         }}
       />
     </>
